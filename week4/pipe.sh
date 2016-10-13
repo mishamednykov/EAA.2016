cc29d27d160910aacca056564d29d9aeb51bb537
John Burns <jzburns@gmail.com>
2016-10-13 10:33:37 +0100

    pipe question

---
+++ week4/pipe.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ps -ef | grep mysql
+
+#ps -ef prints a screenshot of every process in the system
+# grep mysql searches for the text mysql 
+# where does this come from? from the ps -ef |
+
+
+#new commans
+#searches all the running processes looking for the mysql process
